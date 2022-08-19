19f937084fdfb9b78809fbdec349cb5c65cb9ed7
lanaalamsyah <lanaalamsyah04@gmail.com>
2022-08-19 16:46:57 +0700

    2323

--- routes/web.php
+++ routes/web.php
@@ -75,5 +75,5 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 Route::resource('/dashboard/posts', DashboardPostController::class)
 ->middleware('auth');
 
-// Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
-// ->middleware('auth');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
+->middleware('auth');
