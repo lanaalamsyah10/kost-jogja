34fddcd3da9dd9ac226d70b423b3dd82859b2669
lanaalamsyah <lanaalamsyah04@gmail.com>
2022-08-19 15:52:14 +0700

    coba

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
             <h1 class="mb-3">{{ $post->title }}</h1>
           <p>By. <a href="/posts? author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a>
                 in
-              <a href="//posts? category={{ $post->category->slug }}"class="text-decoration-none">{{ 
+              <a href="/posts? category={{ $post->category->slug }}"class="text-decoration-none">{{ 
               $post->category->name }}</p></a>
 
               @if($post->image)
