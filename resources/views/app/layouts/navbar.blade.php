8c809c057df9b13de6393d70dd14eafd2bd16e09
faiq04echo <naufalgame04@gmail.com>
2022-08-31 15:22:16 +0700

    nyusahi

--- resources/views/app/layouts/navbar.blade.php
+++ resources/views/app/layouts/navbar.blade.php
@@ -8,28 +8,33 @@
 
         <nav id="navbar" class="navbar">
             <ul>
-                <li><a class="nav-link scrollto" href="
-                    @if(Request::is('pesan*'))
-                        /
+                <li><a class="nav-link scrollto"
+                        href="
+                    @if (Request::is('pesan*')) /
                     @else
-                        #hero
-                    @endif
-                    ">Home</a></li>
-                <li><a class="nav-link scrollto" href="
-                    @if(Request::is('pesan*'))
-                        /
+                        #hero @endif
+                    ">Home</a>
+                </li>
+                <li><a class="nav-link scrollto"
+                        href="
+                    @if (Request::is('pesan*')) /
                     @else
-                        #about
-                    @endif
-                    ">About</a></li>
-                <li><a class="nav-link scrollto {{ Request::is('pesan*') ? 'active' : '' }}" href="#portfolio">Kost</a></li>
+                        #about @endif
+                    ">About</a>
+                </li>
+                <li><a class="nav-link scrollto {{ Request::is('pesan*') ? 'active' : '' }}" href="#portfolio">Kost</a>
+                </li>
                 <li><a class="nav-link scrollto " href="#services">Kategori</a></li>
                 <li><a class="nav-link scrollto" href="#team">Pemilik</a></li>
                 <li class="dropdown"><a href="/login"><span>Login</span> <i class="bi bi-chevron-down"></i></a>
-                  <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
 
-            <i class="bi bi-list mobile-nav-toggle"></i>
+                <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
 
     </div>
 </header><!-- End Header -->
+
+
+
+{{-- hahahahaha --}}
